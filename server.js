085601a6de67a021ff23c15dcbbda8287c644a86
jjbeck <jordan_becker@brown.edu>
2021-02-03 16:43:40 -0500

    added date scalar type and validation for fname and lname

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require('express');
 const { ApolloServer, UserInputError } = require('apollo-server-express');
 const fs = require('fs');
 const { getMaxListeners } = require('process');
+const { GraphQLScalarType } = require('graphql');
 
 
 allUserInfo = [{
+    id: 1,
     email: 'jordanforemail', 
     fname: 'jordan', 
     lname: 'becker',
     age: 26,
 }];
+
+const UserAquireDate = new GraphQLScalarType({
+    name: 'UserAquireDate',
+    description: 'A Date() type in GraphQL as scalar',
+    serialize(value) {
+        return value.toISOString();
+    },
+});
 
 const resolvers = {
     Query: {
@@ -23,6 +33,8 @@ const resolvers = {
 
 function setUserInfo(_, { info }) {
   validateUserInfo(info);
+  info.id = allUserInfo.length + 1;
+  info.signUpDate = new Date();
   allUserInfo.push(info);
   return info;
 }
@@ -36,12 +48,19 @@ const server = new ApolloServer({
 })
 
 function validateUserInfo(info) {
-    console.log(info);
     const errors = [];
-    const emailExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    const isValidEmail =  emailExpression.test(String(info.email).toLowerCase())
+    const emailExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const nameExpression = /^[a-zA-Z]+$/
+
+    const isValidEmail =  emailExpression.test(String(info.email).toLowerCase());
     if (!isValidEmail) errors.push('Please enter a valid email');
+
+    const isValidfName = nameExpression.test(String(info.fname).toLowerCase());
+    if (!isValidfName) errors.push('Please only use letters when entering first name');
+
+    const isValidlName = nameExpression.test(String(info.lname).toLowerCase());
+    if (!isValidlName) errors.push('Please only use letters when entering lastname');
 
     if (errors.length > 0) {
         throw new UserInputError('Invalid input(s)', { errors });
