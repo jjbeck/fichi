16ffff823b412a5d7d8b9fcdf2798c0e4f057d98
Mauricio Torrejon <mptorrejon@users.noreply.github.com>
2021-02-21 23:06:50 -0500

    added early-access-component

--- ui/src/containers/signup/signup.container.jsx
+++ ui/src/containers/signup/signup.container.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './signup.css';
+import EarlyAccessButtonComponent from '../../components/early-access-button/early-access-button.component.jsx';
 
 export default class SignupContainer extends React.Component {
     render() {
@@ -10,9 +11,7 @@ export default class SignupContainer extends React.Component {
                     If you would like to receive updates on our launch, access to exlusive content, and early user benefits, enter your email.
                     </p>
                     <p className="button-wrapper">
-                        <button className="filled">
-                            Give me early access
-                        </button>
+                        <EarlyAccessButtonComponent />
                         <button className="ghost">
                             Contact Us
                         </button>
