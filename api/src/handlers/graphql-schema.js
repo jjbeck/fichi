816bbcc35a51c4c2c861a636e1dbb805730963b7
jjbeck <jordan_becker@brown.edu>
2021-05-26 17:07:52 -0400

    create react app and neo4j

--- api/src/handlers/graphql-schema.js
+++
@@ -1,8 +0,0 @@
-const neo4jgraphql = require('neo4j-graphql-js');
-const fs = require('fs');
-const path = require('path');
-
-const typeDefs = fs.readFileSync(path.resolve(__dirname, './schema.graphql')).toString('utf-8');
-
-
-module.exports = { typeDefs };
